37cc26d130bade7a89b852e0cd2de18be67e0157
ayequill <ayequillgh@gmail.com>
2023-01-11 09:37:46 +0000

    fixed copy function and alerts

---
+++ src/js/alerts.js
@@ -0,0 +1,15 @@
+export const copyAlert = {
+  text: "Copied!",
+  duration: 3000,
+  destination: "#",
+  newWindow: true,
+  close: true,
+  gravity: "top", 
+  position: "left", 
+  stopOnFocus: true, 
+  style: {
+    background: "#353535",
+    borderRadius: "6px",
+  },
+  onClick: function () {},
+};
--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,54 @@
-const colorInput = document.getElementById('userInput')
-const type = document.getElementById('schemeType')
-const colorBox = document.querySelector('.color-box')
-let colorArr = []
+import { copyAlert } from "./alerts.js";
+const colorInput = document.getElementById("userInput");
+const type = document.getElementById("schemeType");
+const colorBox = document.querySelector(".color-box");
+let colorArr = [];
 
 // using this as a placeholder when the page loads
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic&count=6')
-    .then(res => res.json())
-    .then(data => {
-        colorArr = data.colors.map((color)=> {
-            return color.hex.value
-         })
-         renderColors()
-    })
+fetch(
+  "https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic&count=6"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    colorArr = data.colors.map((color) => {
+      return color.hex.value;
+    });
+    renderColors();
+  });
 
 // added event listeners
-document.body.addEventListener('click', (e)=>{
-    e.preventDefault()
-    if (e.target.id === 'getBtn'){
-        fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.replace('#','')}&format=json&mode=${type.value}&count=6`)
-        .then(res => res.json())
-        .then(data => {
-            // pushed all color scheme to my array to render later
-             colorArr = data.colors.map((color)=> {
-               return color.hex.value
-            })
-            renderColors()
-        })
-    }
-    // copy to clipboard
-const colorValue = document.getElementById('color-value')
-    if (e.target.id === 'color-value'){
-        console.log('click');
-        navigator.clipboard.writeText(colorValue.textContent);
-    }
-})
+document.body.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.id === "getBtn") {
+    fetch(
+      `https://www.thecolorapi.com/scheme?hex=${colorInput.value.replace(
+        "#",
+        ""
+      )}&format=json&mode=${type.value}&count=6`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // pushed all color scheme to my array to render later
+        colorArr = data.colors.map((color) => {
+          return color.hex.value;
+        });
+        renderColors();
+      });
+  }
+
+  // copy to clipboard
+  if (e.target.dataset.color) {
+    navigator.clipboard.writeText(e.target.dataset.color);
+    Toastify(copyAlert).showToast();
+  }
+});
 
 function renderColors() {
+  let html = "";
 
-    let html = ''
+  colorArr.map((color) => {
+    html += `<div style="background-color:${color};" class="colors"><p data-color="${color}" class="color-value">${color}</p></div>`;
+  });
 
-    colorArr.map((color)=>{
-        html += `<div style="background-color:${color};" class="colors"><p id="color-value">${color}</p></div>`
-    })
-    
-   colorBox.innerHTML = html
+  colorBox.innerHTML = html;
 }
